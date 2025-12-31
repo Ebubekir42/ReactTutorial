63e984c985b6222a551630b7cded7e4a691a7f05
Ebubekir42 <ebubekirturkk2001@gmail.com>
2025-12-31 23:33:46 +0300

    ES6 - Arrow Function

--- src/ES6.jsx
+++ src/ES6.jsx
@@ -24,4 +24,30 @@ class Model extends Car{
 const mycar = new Model("Ford", "Mustang");
 const text = mycar.show();
 
+// ES6 - Arrow Function
+const hello = () => {
+    return "Hello World";
+}
+const hello2 = () => "Hello World";
+const hello3 = val => `Hello ${val}`;
+const hello4 = (val1, val2) => `Hello ${val1}, ${val2}`;
 
+class Header{
+    constructor(){
+        this.color = "red";
+    }
+    changeColor = function(){
+        document.getElementById("root").innerHTML += this;
+    }
+
+    changeColor2 = () => {
+        document.getElementById("root").innerHTML += this;
+    }
+}
+
+const myHeader = new Header();
+window.addEventListener("load", myHeader.changeColor); // object Window
+window.addEventListener("load", myHeader.changeColor2); // object Object
+
+document.getElementById("btn").addEventListener("click", myHeader.changeColor); // object HTMLButtonElement
+document.getElementById("btn").addEventListener("click", myHeader.changeColor2); // object Object
