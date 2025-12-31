c8af0d36b6a67e9155e68d5f7cdea51086bf514d
Ebubekir42 <ebubekirturkk2001@gmail.com>
2025-12-31 23:17:36 +0300

    ES6- Class

---
+++ src/ES6.jsx
@@ -0,0 +1,27 @@
+// ES6 - Class
+
+class Car{
+    constructor(name){
+        this.brand = name;
+    }
+
+    present(){
+        return `I have a ${this.brand}`;
+    }
+}
+
+class Model extends Car{
+    constructor(name, mod){
+        super(name);
+        this.model = mod;
+    }
+
+    show(){
+        return `${this.present()}, it is a ${this.model}`;
+    }
+}
+
+const mycar = new Model("Ford", "Mustang");
+const text = mycar.show();
+
+
