ff9e6b295a83cc4d515b8aac75e902a613da2bce
Ebubekir42 <ebubekirturkk2001@gmail.com>
2026-01-01 23:51:36 +0300

    Select

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const MyForm = () => {
     const [name, setName] = useState("John");
     const [mytxt, setMytxt] = useState("Text");
+    const [myCar, setMyCar] = useState("Volvo");
 
     const handleChange = (e) => {
         setName(e.target.value);
@@ -10,6 +11,9 @@ const MyForm = () => {
 
     const handleChange2 = (e) => {
         setMytxt(e.target.value);
+    }
+    const handleChange3 = (e) => {
+        setMyCar(e.target.value);
     }
 
     const handleSubmit = (e) => {
@@ -25,6 +29,11 @@ const MyForm = () => {
             <p>Current value: {name}</p>
             <textarea value={mytxt} onChange={handleChange2} />
             <p>Current value: {mytxt}</p>
+            <select value={myCar} onChange={handleChange3}>
+                <option value="Ford">Ford</option>
+                <option value="Volvo">Volvo</option>
+                <option value="Fiat">Fiat</option>
+            </select>
             <input type="submit" />
         </form>
     );
